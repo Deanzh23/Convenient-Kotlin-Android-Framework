cb09d4ce60fdcd3ccd9768cc2932a610d1a3b3ac
Dean <410082323@qq.com>
2017-12-23 15:02:21 +0800

    Dean
    commit.

--- convenient/src/main/java/com/dean/fw/convenient/network/DefaultHttpConnection.kt
+++ convenient/src/main/java/com/dean/fw/convenient/network/DefaultHttpConnection.kt
@@ -2,6 +2,7 @@ package com.dean.fw.convenient.network
 
 import android.util.Log
 import com.dean.fw.convenient.network.listener.OnConnectionListener
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
@@ -9,6 +10,7 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
+import java.util.*
 
 /**
  * Http请求超类
@@ -94,6 +96,64 @@ open class DefaultHttpConnection {
         }
     }
 
+    private fun sendHttpPost(basicURL: String, headerParams: LinkedHashMap<String, Any>, urlParams: Any, bodyParams: Any?, encoding: String, connectTimeout: Int,
+                             isUseCache: Boolean, onConnectionListener: OnConnectionListener) {
+
+        var httpURLConnection: HttpURLConnection? = null
+        var inputStream: InputStream? = null
+        var inputStreamReader: InputStreamReader? = null
+        var reader: BufferedReader? = null
+
+        try {
+            // 配置参数以后的url部分
+            val httpUrl = getHttpURL(basicURL, urlParams)
+            val url = URL(httpUrl)
+            httpURLConnection = url.openConnection() as HttpURLConnection
+            // 设置连接规则以及连接头参数
+            setHttpURLConnectionConfig(httpURLConnection, "POST", encoding, connectTimeout, isUseCache, headerParams)
+            // 发起连接
+            httpURLConnection.connect()
+
+            // 构建body体
+            var bodyBuffer: StringBuffer? = null
+            if (bodyParams != null) {
+                bodyBuffer = StringBuffer()
+
+                // 是Map结构
+                if (bodyParams is Map<*, *> && !bodyParams.isEmpty()) {
+                    var json = JSONObject()
+
+                    bodyParams.entries.forEach {
+
+                        val key = it.key.toString()
+                        val value = it.value
+
+                        if (value is String)
+                            json.put(URLEncoder.encode(key), URLEncoder.encode(value))
+                        else
+                            json.put(URLEncoder.encode(key), value)
+                    }
+
+                    bodyBuffer.append(json.toString())
+                }
+                // 是List结构
+                else if (bodyParams is String && !bodyParams.isEmpty())
+                    bodyBuffer.append(bodyParams)
+            }
+
+            // 发送body数据
+            if (bodyBuffer != null && bodyBuffer.length > 0) {
+                val outputStream = httpURLConnection.outputStream
+                outputStream.write(bodyBuffer.toString().toByte())
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+
+        } finally {
+
+        }
+    }
+
     /**
      * 获取配置参数后的最终url
      *
@@ -107,30 +167,26 @@ open class DefaultHttpConnection {
         var tempUrlParams: Any
 
         // 如果参数是由Map传递
-        if (urlParams is Map<*, *>) {
-            tempUrlParams = urlParams as LinkedHashMap<*, *>
+        if (urlParams is LinkedHashMap<*, *> && !urlParams.isEmpty()) {
+            urlParams.entries.forEach {
 
-            if (!tempUrlParams.isEmpty()) {
-                tempUrlParams.entries.forEach {
+                val key = it.key.toString()
+                val value = it.value
 
-                    val key = it.key as String
-                    val value = it.value
+                // basic url后的分隔符
+                if (i == 0)
+                    urlBuilder.append("?")
+                else
+                    urlBuilder.append("&")
+                // 参数key+连接符
+                urlBuilder.append(URLEncoder.encode(key)).append("=")
+                // 参数值
+                if (value is String)
+                    urlBuilder.append(URLEncoder.encode(value))
+                else
+                    urlBuilder.append(value)
 
-                    // basic url后的分隔符
-                    if (i == 0)
-                        urlBuilder.append("?")
-                    else
-                        urlBuilder.append("&")
-                    // 参数key+连接符
-                    urlBuilder.append(URLEncoder.encode(key)).append("=")
-                    // 参数值
-                    if (value is String)
-                        urlBuilder.append(URLEncoder.encode(value))
-                    else
-                        urlBuilder.append(value)
-
-                    i++
-                }
+                i++
             }
         }
         // 如果参数是由List传递
